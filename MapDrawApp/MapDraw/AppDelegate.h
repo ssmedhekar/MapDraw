a12629222d45c7f99ec34bc3bd6508bfa113f97c
Sachin Medhekar <sachinmedhekar@Sachins-MacBook-Pro.local>
2014-11-19 16:53:26 -0800

    added files

---
+++ MapDrawApp/MapDraw/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MapDraw
+//
+//  Created by Sachin Medhekar on 11/19/14.
+//  Copyright (c) 2014 Sachin Medhekar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MapDrawApp/MapDraw/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MapDraw
+//
+//  Created by Sachin Medhekar on 11/19/14.
+//  Copyright (c) 2014 Sachin Medhekar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
